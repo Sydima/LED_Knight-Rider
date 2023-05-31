7ac8fd97073a080136608f070259b80f9900dc5d
Sydima <78255692+Sydima@users.noreply.github.com>
2023-05-31 17:09:58 +0800

    Update For_T0_V0.ino

--- For_T0_V0.ino
+++ For_T0_V0.ino
@@ -1,23 +1,19 @@
-
-void setup() 
-{
+void setup() {
   // put your setup code here, to run once:
-Serial.begin (9600);
-
-// int x;
-
-for (int x=0; x<=100; x++) 
-{
-Serial.println (x);
- }
-
-
+//Serial.begin(9600);
+for (int pin_no=5; pin_no<8; pin_no++){
+//Serial.println  (pin_no);
+pinMode(pin_no, OUTPUT);
+}
 }
 
-
-void loop() 
-{
+void loop() {
   // put your main code here, to run repeatedly:
-
-
+for (int pin_no=5; pin_no<8; pin_no++){
+digitalWrite(pin_no, HIGH);
+delay(1000);
+digitalWrite(pin_no, LOW);
+delay (1000);
+//Serial.println(pin_no);
+}
 }
