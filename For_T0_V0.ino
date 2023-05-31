1819b1b7ca16f0284abe4236f39649f6eaaf703d
Sydima <78255692+Sydima@users.noreply.github.com>
2023-05-31 17:04:27 +0800

    Add files via upload

---
+++ For_T0_V0.ino
@@ -0,0 +1,23 @@
+
+void setup() 
+{
+  // put your setup code here, to run once:
+Serial.begin (9600);
+
+// int x;
+
+for (int x=0; x<=100; x++) 
+{
+Serial.println (x);
+ }
+
+
+}
+
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+
+
+}
